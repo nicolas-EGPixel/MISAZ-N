dcacd9a00d5f6572b690d931b522b0fd63cb7c55
Jaime1945 <jaimejuliancastillo23@cbtis52.edu.mx>
2026-05-28 07:32:32 -0600

    Nuevos estilos, mapa (no funcional), y ya creo

---
+++ lib/pages/perfil.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PerfilPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Tu perfil 👤",
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
