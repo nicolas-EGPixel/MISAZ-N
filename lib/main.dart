3800268228290f0b47706c1e8005c3430ad8affc
MarintiaJAM <marintiaitzellopez23@cbtis52.edu.mx>
2026-05-27 19:30:36 -0600

    Ya se arregló bien, todo en orden

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mi Sazón',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        scaffoldBackgroundColor: Colors.orange.shade50,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          filled: true,
+          fillColor: Colors.white,
+        ),
+      ),
+
       home: LoginPage(),
     );
   }
